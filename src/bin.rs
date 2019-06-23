c017bf62a66f995d3a1a34cf713c141db817e437
Jacob Haslehurst <jacob@haslehurst.net>
2019-06-23 14:53:03 +1000

    add initial command to fetch current price of symbol

--- src/bin.rs
+++ src/bin.rs
@@ -1,13 +1,23 @@
-use clap::{self, App, Arg};
+use clap::{self, App, Arg, SubCommand};
 
 fn main() {
+    let symbol_arg = Arg::with_name("symbol").required(true);
+
     let matches = App::new("Portfolio")
         .version("0.1")
         .author("Jacob Haslehurst <jacob@haslehurst.net>")
-        .arg(Arg::with_name("symbol").required(true))
+        .subcommand(SubCommand::with_name("latest-price").arg(symbol_arg))
         .get_matches();
 
-    let symbol = matches.value_of("symbol").unwrap();
+    match matches.subcommand() {
+        ("latest-price", Some(matches)) => {
+            let symbol = matches.value_of("symbol").unwrap();
 
-    println!("symbol: {}", symbol);
+            let price =
+                portfolio::get_latest_price_for_equity(portfolio::Symbol::new(symbol)).unwrap();
+
+            println!("{}: {}", symbol, price);
+        }
+        (&_, _) => println!("Command not recognised"),
+    };
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,130 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::collections::HashMap;
+use std::env;
+use std::ops::Deref;
+
+use lazy_static::lazy_static;
+use reqwest;
+use serde::Deserialize;
+use serde_aux::field_attributes::deserialize_number_from_string;
+
+lazy_static! {
+    static ref CLIENT: reqwest::Client = reqwest::Client::new();
+    static ref VANTAGE_API_KEY: String =
+        env::var("VANTAGE_API_KEY").expect("`VANTAGE_API_KEY` environment variable must be set");
+}
+
+pub struct Symbol(String);
+impl Symbol {
+    pub fn new<S: Into<String>>(s: S) -> Symbol {
+        Symbol(s.into())
     }
+}
+
+impl Deref for Symbol {
+    type Target = str;
+    fn deref(&self) -> &str {
+        self.0.as_str()
+    }
+}
+
+enum DailyOutputSize {
+    Compact,
+    Full,
+}
+impl DailyOutputSize {
+    fn as_str(&self) -> &'static str {
+        match self {
+            DailyOutputSize::Compact => "compact",
+            DailyOutputSize::Full => "full",
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum ApiError {
+    Reqwest(reqwest::Error),
+}
+impl From<reqwest::Error> for ApiError {
+    fn from(error: reqwest::Error) -> ApiError {
+        ApiError::Reqwest(error)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct TimeSeriesDay {
+    #[serde(
+        rename = "1. open",
+        deserialize_with = "deserialize_number_from_string"
+    )]
+    open: f64,
+    #[serde(
+        rename = "2. high",
+        deserialize_with = "deserialize_number_from_string"
+    )]
+    high: f64,
+    #[serde(rename = "3. low", deserialize_with = "deserialize_number_from_string")]
+    low: f64,
+    #[serde(
+        rename = "4. close",
+        deserialize_with = "deserialize_number_from_string"
+    )]
+    close: f64,
+    #[serde(
+        rename = "5. adjusted close",
+        deserialize_with = "deserialize_number_from_string"
+    )]
+    adjusted_close: f64,
+    #[serde(
+        rename = "6. volume",
+        deserialize_with = "deserialize_number_from_string"
+    )]
+    volume: f64,
+    #[serde(
+        rename = "7. dividend amount",
+        deserialize_with = "deserialize_number_from_string"
+    )]
+    dividend_amount: f64,
+    #[serde(
+        rename = "8. split coefficient",
+        deserialize_with = "deserialize_number_from_string"
+    )]
+    split_coefficient: f64,
+}
+
+#[derive(Debug, Deserialize)]
+struct TimeSeriesDailyResponse {
+    #[serde(rename = "Meta Data")]
+    metadata: serde_json::Value,
+    #[serde(rename = "Time Series (Daily)")]
+    time_series: HashMap<String, TimeSeriesDay>,
+}
+
+fn get_time_series_daily(
+    client: &reqwest::Client,
+    symbol: Symbol,
+    output_size: DailyOutputSize,
+) -> Result<TimeSeriesDailyResponse, ApiError> {
+    client
+        .get("https://www.alphavantage.co/query")
+        .query(&[
+            ("function", "TIME_SERIES_DAILY_ADJUSTED"),
+            ("symbol", &*symbol),
+            ("apikey", &*VANTAGE_API_KEY),
+            ("outputsize", output_size.as_str()),
+        ])
+        .send()
+        .and_then(|resp| resp.error_for_status())
+        .and_then(|mut resp| resp.json())
+        .map_err(|err| err.into())
+}
+
+pub fn get_latest_price_for_equity(symbol: Symbol) -> Result<f64, ApiError> {
+    let result = get_time_series_daily(&CLIENT, symbol, DailyOutputSize::Compact)?;
+
+    Ok(result
+        .time_series
+        .iter()
+        .max_by_key(|&(date, data)| date)
+        .map(|(date, data)| data.close)
+        .unwrap())
 }
